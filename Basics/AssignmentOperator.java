4765d933260a853e3d36510bfa39e5edc6e65bde
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2023-09-17 01:00:19 +0600

    Create AssignmentOperator.java

---
+++ Basics/AssignmentOperator.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AssignmentOperator {
+    public static void main(String args[]){
+        int a=10;
+        a+=10;
+        System.out.println(a);
+
+        int b=21;
+        b-=9;
+        System.out.println(b);
+    }
+}
