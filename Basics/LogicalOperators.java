041e325b66016b72174e158b6858ce10393e3245
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2023-09-17 01:00:31 +0600

    Create LogicalOperators.java

---
+++ Basics/LogicalOperators.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class LogicalOperators {
+    public static void main(String args[]){
+        //Logical AND
+        System.out.println((3>1) && (5>2));
+
+        //logical OR
+        System.out.println((7>2) || (9<2));
+
+        //logical NOt
+        System.out.println(!(3>2));
+    }
+}
