7e1fcc8675c4b862bf5bdbb260fe8ead5fb3182c
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2023-09-17 01:00:37 +0600

    Create Operator2.java

---
+++ Basics/Operator2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Operator2 {
+    public static void main(String args[]){
+        int x=20, y=50, z=100;
+        if(x>y && y>z){
+            System.out.println("HELLO!");
+        }
+        if(z>y && z<x){
+            System.out.println("Yap!");
+        }
+        if (y+200<x && y+100<z) {
+            System.out.println("Hawayein");
+        }
+
+    }
+}
