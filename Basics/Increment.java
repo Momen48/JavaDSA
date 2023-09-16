5bdd2cba1ad5da23d578c5bb586ae8ec4ede96c3
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2023-09-17 01:00:23 +0600

    Create Increment.java

---
+++ Basics/Increment.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Increment {
+    public static void main(String args[]){
+        int a = 10;
+        int b=++a;
+
+        System.out.println("Value of a: "+ a);
+        System.out.println("Value of b: "+ b);
+
+        int c = 15;
+        int d=c++;
+
+        System.out.println("Value of c: "+ c);
+        System.out.println("Value of d: "+ d);
+    }
+}
