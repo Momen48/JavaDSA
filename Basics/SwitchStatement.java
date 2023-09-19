568bfb22cad8a781ac9ad07ebd35ee9f88c03e9f
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2023-09-19 22:28:04 +0600

    Create SwitchStatement.java

---
+++ Basics/SwitchStatement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class SwitchStatement {
+    public static void main(String args[]){
+        int n = 2;
+        switch(n){
+            case 1: System.out.println("Birani");
+            break;
+            case 2: System.out.println("Fried Rice");
+            break;
+            case 3: System.out.println("Murag Polao");
+            break;
+            default: System.out.println("Daal vaat");
+
+        }
+    }
+}
