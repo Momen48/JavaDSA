1e07cb2d444e2c982b68a4e7707d88313f1f0592
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2023-09-21 01:10:47 +0600

    Create FeverCheck.java

---
+++ Basics/FeverCheck.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class FeverCheck {
+    public static void main(String args[]){
+        double temp = 103.5;
+        if(temp>100){
+            System.out.println("I have a fever!");
+        }
+        else{
+            System.out.println("Nothing! just came from outside on a sunny day");
+        }
+    }
+}
