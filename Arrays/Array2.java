5c8d5f4f676c9247e68902dc5a57c96beeafc8e7
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2023-09-26 01:08:19 +0600

    Update Array2.java

--- Arrays/Array2.java
+++ Arrays/Array2.java
@@ -1,6 +1,21 @@
 import java.util.*;
 public class Array2 {
     public static void main(String args[]){
+        int marks [] = new int[50];
+        System.out.println("Array Lenght: "+marks.length);
+        Scanner sc = new Scanner(System.in);
+        marks[0]= sc.nextInt();
+        marks[1]= sc.nextInt();
+        marks[2]= sc.nextInt();
 
+        System.out.println("Phy"+marks[0]);
+        System.out.println("Chem"+marks[1]);
+        System.out.println("Math"+marks[2]);
+
+        int avg = (marks[0]+marks[1]+marks[2])/3;
+        System.out.println("Percentage is :" + avg + "%");
+
+        int numbers [] = {1,3,4};
+        System.out.println(numbers[2]);
     }
 }
