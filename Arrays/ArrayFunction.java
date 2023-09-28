40e63305c6391c5eefdff74038d559d28a141efe
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2023-09-28 15:22:11 +0600

    Update ArrayFunction.java

--- Arrays/ArrayFunction.java
+++ Arrays/ArrayFunction.java
@@ -1,6 +1,16 @@
-
+import java.util.*;
 public class ArrayFunction {
+    public static void update(int mark[]){
+        for (int i= 0 ; i<mark.length;i++){
+            mark[i]+=1;
+        }
+    }
     public static void main(String args[]){
-        
+        int mark[] = {88, 77,98};
+        update(mark);
+        for(int i = 0; i<mark.length;i++){
+            System.out.print(mark[i] +" ");
+        }
+        System.out.println();
     }
 }
