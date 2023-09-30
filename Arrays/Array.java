a477de74c840b0114ff76fb30e97b39755578654
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2023-09-30 11:38:49 +0600

    Update Array.java

--- Arrays/Array.java
+++ Arrays/Array.java
@@ -1,7 +1,13 @@
 import java.util.*;
 
 public class Array {
-    public static void main(){
-        System.out.println("");
+    public static int Function(int num[],int key){
+
+
+        return -1;
+    }
+    public static void main(String args []){
+        int num[] = {2,4,6,8,10,12,14,16};
+        int key = 10;
     }
 }
