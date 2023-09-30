7b7b2588f9f1037d308aa0ed4dbf4dbcc1fb7e25
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2023-09-30 11:38:55 +0600

    Create aarrBoiler.java

---
+++ Arrays/aarrBoiler.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class aarrBoiler {
+    public static int Function(int num[],int key){
+
+
+        return -1;
+    }
+    public static void main(String args []){
+        int num[] = {2,4,6,8,10,12,14,16};
+        int key = 10;
+    }
+}
