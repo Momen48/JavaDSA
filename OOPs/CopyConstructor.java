b32bd9cda2689e32689a929056ffa8cdf1380b7d
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2024-07-26 00:27:24 +0600

    Create CopyConstructor.java

---
+++ OOPs/CopyConstructor.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class CopyConstructor {
+    public static void main(String args[]){
+        System.out.println("Constructor");
+
+        Student s1=new Student();
+        s1.name = "Momen";
+        s1.roll = 48;
+        s1.password = "abcd";
+        
+        s1.marks[0] = 100;
+        s1.marks[1] = 90; 
+        s1.marks[2] = 80;
+
+        Student s2 = new Student(s1);
+        s2.password = "xyz"; 
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(s2.marks[i]);
+        } 
+    }
+}
+
+class Student{
+    int roll;
+    String name;
+    String password;
+    int marks[]; 
+
+    // copy constructor 
+Student(Student s1){
+    marks = new int[3];
+    this.name = s1.name;
+    this.roll = s1.roll;
+    this.marks = s1.marks;
+    
+}
+Student(){
+    marks = new int[3];
+}
+
+// parameterized constructor
+    Student(String name){
+        this.name=name;
+        marks = new int[3];
+    }
+    Student(int roll){
+        this.roll=roll;
+        marks = new int[3];
+    }
+}
