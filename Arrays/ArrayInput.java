19fca54b6afba5497348153713127570f1012903
Muhammad Nurul Momen <momeniiuc7@gmail.com>
2025-02-21 10:08:59 +0600

    1

---
+++ Arrays/ArrayInput.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ArrayInput {
+    public static int Function(int num[],int key){
+
+
+        return -1;
+    }
+    public static void main(String args []){
+        int marks[] = new int[100];
+
+        Scanner sc = new Scanner(System.in);
+
+      //  int phy = sc.nextInt();
+    }
+}
