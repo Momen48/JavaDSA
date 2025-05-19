8b9a7623fcdbfedb3fbd0e313f44498a5f9cb3cf
Muhammad Nurul Momen <39500000+Momen48@users.noreply.github.com>
2025-05-19 13:29:31 +0600

    Update String_1.java

--- 2. String/String_1.java
+++ 2. String/String_1.java
@@ -4,11 +4,13 @@ public class String_1{
     
     public static void main(String[] args) {
         
-        System.out.println("Enter Your Name : ");
-        
-        Scanner sc = new Scanner(System.in);
-        String name  = sc.nextLine();
+    //    System.out.println("Enter Your Name : ");    
+    //    Scanner sc = new Scanner(System.in);
+    //    String name  = sc.nextLine();
 
-        System.out.println("Your name is: " + name);
+          String name  = "Tony Starkb";
+
+          System.out.println("Your name is: " + name);
+          System.out.println(name.length());
     }
 }
