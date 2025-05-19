6b93c3d35c037947ce216af9627ea779aa6271db
Muhammad Nurul Momen <39500000+Momen48@users.noreply.github.com>
2025-05-19 13:21:59 +0600

    Create Ex_8_ReverseArray.java

---
+++ 1. Array/Ex_8_ReverseArray.java
@@ -0,0 +1,16 @@
+public class Ex_8_ReverseArray {
+
+    public static int ReverseArray(int arr[]){
+        int start = arr[0];
+        int end = arr.length-1;
+        while (start < end ){
+            //swap 
+            int temp = 0;
+            end = 
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr [] = {2,4,6,8,10,12};
+    }
+}
