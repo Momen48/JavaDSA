3321d717afb608e2b893eb4ea86af9e54b8379b9
Muhammad Nurul Momen <39500000+Momen48@users.noreply.github.com>
2025-05-19 13:22:31 +0600

    Create Ex_3_nestedScope.java

---
+++ 1. Array/Ex_3_nestedScope.java
@@ -0,0 +1,11 @@
+public class Ex_3_nestedScope {
+    public static void main(String[] args) {
+        int x=10;
+        if (x==10) {
+            int y=20;
+            System.out.println("x="+x+",y="+y);
+        }
+       // y=5;
+       // System.out.println("x="+x+",y="+y);
+    }
+}
