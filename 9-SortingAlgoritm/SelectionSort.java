f0da9638c527405faaa63380a618c4ffbac0277a
Muhammad Nurul Momen <39500000+Momen48@users.noreply.github.com>
2025-05-19 13:21:53 +0600

    Update SelectionSort.java

--- 9-SortingAlgoritm/SelectionSort.java
+++ 9-SortingAlgoritm/SelectionSort.java
@@ -1,8 +1,23 @@
 import java.util.*;
 
+// Pick The Smallest Of any array, and put it at the beginning
 public class SelectionSort {
 
     public static void Selsort(int arr[]) {
+        for (int i = 0; i < arr.length - 2; i++) {
+            int min = i;
+            // 5 enters as arr[0] = 5
+            for (int j = i + 1; j < arr.length; j++) {
+                // arr[j] = 4, then 1, then 3, then 2
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            // swap outside second loop
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
     }
 
     public static void printF(int arr[]) {
@@ -14,6 +29,7 @@ public class SelectionSort {
 
     public static void main(String args[]) {
         int arr[] = { 5, 4, 1, 3, 2 };
+        // System.out.println(arr.length);
         Selsort(arr);
         printF(arr);
     }
