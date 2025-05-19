0d00774a74ded05713769fe041737a268a9e5f79
Muhammad Nurul Momen <39500000+Momen48@users.noreply.github.com>
2025-05-19 13:21:57 +0600

    Create String_1.java

---
+++ 2. String/String_1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class String_1{
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Enter Your Name : ");
+        
+        Scanner sc = new Scanner(System.in);
+        String name  = sc.nextLine();
+
+        System.out.println("Your name is: " + name);
+    }
+}
