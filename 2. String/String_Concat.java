f187a4da2b582183b4fc34a701b0235d097150d5
Muhammad Nurul Momen <39500000+Momen48@users.noreply.github.com>
2025-05-19 13:45:33 +0600

    Create String_Concat.java

---
+++ 2. String/String_Concat.java
@@ -0,0 +1,8 @@
+public class String_Concat {
+    public static void main(String[] args) {
+        String fName= "Muhammad Nurul";
+        String lName= "Momen";
+        String fullName= fName + " " + lName;
+        System.out.println(fullName);
+    }
+}
