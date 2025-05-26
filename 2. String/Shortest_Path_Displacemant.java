e50e7b360322c9d25c1bd29a0ca34925e35e60bb
Muhammad Nurul Momen <39500000+Momen48@users.noreply.github.com>
2025-05-26 20:19:22 +0600

    Update Shortest_Path_Displacemant.java

--- 2. String/Shortest_Path_Displacemant.java
+++ 2. String/Shortest_Path_Displacemant.java
@@ -1,25 +1,25 @@
 public class Shortest_Path_Displacemant {
     
-    public static float    getPath(String path){
-        int x=0, y=0;
-        for (int i = 0; i<path.length();i++){
+    public static float  getPath(String path) {
+        int x = 0, y = 0;
+        for (int i = 0; i < path.length(); i++) {
             char dir = path.charAt(i);
-            if (dir == "S"){
+            if (dir == 'S') {
                 y--;
-            }
-            else if (dir == "N"){
+            } else if (dir == 'N') {
                 y++;
-            }
-            else if (dir == "W"){
+            } else if (dir == 'W') {
                 x--;
-            }
-            else {
+            } else {
                 x++;
             }
-
         }
-    public static void main(String[] args) {
-        String path = "WNEENESENNN";
-        System.out.println(getPath(path));
+        int X2 = x*x;
+        int Y2 = y*y;
+        return (float)Math.sqrt(X2+Y2);
     }
-}
+        public static void main (String[]args){
+            String path = "WNEENESENNN";
+            System.out.println(getPath(path));
+        }
+    }
